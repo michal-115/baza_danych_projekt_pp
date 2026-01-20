e618bc95935e281e6118c01f7440eb9a82ba5482
Michał <92009@student.pb.edu.pl>
2026-01-20 18:12:00 +0100

    dodano plik main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "menu.h"
 
-int main(void)
-{
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+    Node* head = NULL;
+    menu(&head, argv[1]);
+    freeList(head);
     return 0;
 }
